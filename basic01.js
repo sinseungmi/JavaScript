f299b187b6bd16e80f441a6912fdc839874c47dc
sinseungmi <tlstmdal3@naver.com>
2022-02-01 21:46:04 +0900

    구구단

--- basic01.js
+++ basic01.js
@@ -1,9 +1,9 @@
 alert("세미콜론 지우기");
 alert("에러가 발생합니다.");
 [1, 2].forEach(alert)
-console.log(1);
 
-foo();
-var foo = () => {
-    console.log(1230);
+for(let i = 1; i < 10; i++) {
+    for(let j = 1; j < 10; j++) {
+        console.log(i + "X" + j + "= " + i * j);
+    }
 }
